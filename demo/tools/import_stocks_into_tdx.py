d515488c81f5b7042992854bb0a96ca42c55033d
Dan Wei <ww_dan@sina.com>
2017-08-13 15:14:55 +0800

    Import result of stock searching into THS

--- demo/tools/import_stocks_into_tdx.py
+++ demo/tools/import_stocks_into_tdx.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import string
 import json
@@ -105,7 +107,7 @@ def import_stocks_into_tdx(tdx_dir,
                                     stock_exchange = stock.split(',')[1].split('.')
                                     if stock_exchange[1] == 'SH\n':
                                         stocks_in_board.append('1' + stock_exchange[0] + '\n')
-                                    elif stock_exchange[1] == 'SZ\n':
+                                    elif stock_exchange[1] == 'SZ\n'or stock_exchange[1] == 'CYB\n':
                                         stocks_in_board.append('0' + stock_exchange[0] + '\n')
 
                         tdx_board_name = period_v + strategy + date.replace('-', '')
@@ -122,6 +124,85 @@ def import_stocks_into_tdx(tdx_dir,
 
     cfg_file.write(cfg_string)
     cfg_file.close()
+
+
+def import_stocks_into_ths(ths_dir,
+                           stock_search_dir,
+                           periods,
+                           strategies,
+                           start_day,
+                           end_day,
+                           cur_end = None,
+                           offset_in_period = None):
+
+    stocks_in_board = ''
+    for period_k, period_v in periods.items():
+        p_dir = os.path.join(stock_search_dir, period_k)
+        if not os.path.exists(p_dir):
+            print("The stock search results directory, %s, not exist." % p_dir)
+            continue
+
+        for strategy in strategies:
+            p_s_dir = os.path.join(p_dir, strategy)
+            if not os.path.exists(p_s_dir):
+                print("The stock search results directory, %s, not exist." % p_s_dir)
+                continue
+            date_list = os.listdir(p_s_dir)
+            for date in date_list:
+                if start_day <= date <= end_day:
+                    p_s_d_dir = os.path.join(p_s_dir, date)
+                    csv_list = os.listdir(p_s_d_dir)
+                    if len(csv_list):
+                        for csv in csv_list:
+                            csv_path = os.path.join(p_s_d_dir, csv)
+                            with open(csv_path, 'r') as csv_file:
+                                stocks = csv_file.readlines()
+                                stocks = stocks[1:]
+                                for stock in stocks:
+                                    stock_exchange = stock.split(',')[1].split('.')
+                                    if stock_exchange[1] == 'SH\n':
+                                        stocks_in_board += '17:' + stock_exchange[0] + ','
+                                    elif stock_exchange[1] == 'SZ\n' or stock_exchange[1] == 'CYB\n':
+                                        stocks_in_board += '33:' + stock_exchange[0] + ','
+
+    stocks_in_board += '\n'
+    if not os.path.exists(ths_dir):
+        print("The tdx directory, %s, not exist." % ths_dir)
+        exit(1)
+
+    cfg_path = os.path.join(ths_dir, 'StockBlock.ini')
+    if not os.path.exists(cfg_path):
+        return
+    else:
+        cfg_file = open(cfg_path, 'r')
+        cfglines = cfg_file.readlines()
+        cfg_file.close()
+
+        board_num = len(cfglines) - cfglines.index('[BLOCK_NAME_MAP_TABLE]\n') - 1
+        if board_num < 7:
+            cfglines.insert(cfglines.index('[BLOCK_NAME_MAP_TABLE]\n') + 1 + board_num,
+                            str(23 + board_num) + "=" + start_day.replace('-', ''))
+            cfglines.insert(cfglines.index('[BLOCK_STOCK_CONTEXT]\n') + 2 + board_num,
+                            str(23 + board_num) + "=" + stocks_in_board)
+        else:
+            board_id_list = [item[0:2] for item in
+                            cfglines[cfglines.index('[BLOCK_NAME_MAP_TABLE]\n') + 1:]]
+            board_id_list = filter(lambda x:x, board_id_list)
+            board_name_list = [item[-9: -1] for item in
+                              cfglines[cfglines.index('[BLOCK_NAME_MAP_TABLE]\n') + 1:]]
+            board_name_list = filter(lambda x: x, board_name_list)
+            replace_index = cfglines.index('[BLOCK_NAME_MAP_TABLE]\n') + board_name_list.index(min(board_name_list)) + 1
+            cfglines.remove(cfglines[replace_index])
+            cfglines.insert(replace_index,
+                            board_id_list[board_name_list.index(min(board_name_list))] + "=" + start_day.replace('-', '') + '\n')
+            replace_index = cfglines.index('[BLOCK_STOCK_CONTEXT]\n') + board_name_list.index(min(board_name_list)) + 2
+            cfglines.remove(cfglines[replace_index])
+            cfglines.insert(replace_index,
+                            board_id_list[board_name_list.index(min(board_name_list))] + "=" + stocks_in_board)
+
+        cfg_file = open(cfg_path, 'w')
+        cfg_file.writelines(cfglines)
+        cfg_file.close()
 
 def get_cur_weekend_and_offset(current_day, week_dir):
     year = current_day.year
@@ -167,27 +248,57 @@ def daily_import_stocks_into_tdx(week_dir,
                            str(weekend),
                            offset)
 
+def daily_import_stocks_into_ths(week_dir,
+                                 ths_dir,
+                                 stock_search_dir,
+                                 periods,
+                                 strategies):
+    current_day = datetime.date.today()
+    last_day = current_day - datetime.timedelta(days=1)
+    weekend, offset = get_cur_weekend_and_offset(last_day, week_dir)
+    uipath = unicode(ths_dir, "utf8")
+    import_stocks_into_ths(uipath,
+                           stock_search_dir,
+                           periods,
+                           strategies,
+                           str(last_day),
+                           str(weekend),
+                           str(weekend),
+                           offset)
+
+
 '''
 import_stocks_into_tdx('D:\\new_qlzq_v6\T0002\\blocknew',
                        'D:\dan\stock\py_stock\quantdigger\candicates',
                        {'1DAY': '1D',},
-                       ['MACD_MA8'],
-                       '2016-01-10',
-                       '2016-12-07'
+                       ['MACD_MA8', ],
+                       '2017-01-03',
+                       '2017-08-07'
                        )
-
 
 
 remove_stocks_from_tdx('D:\\new_qlzq_v6\T0002\\blocknew',
                        {'1DAY': '1D',},
                        ['ZT62808DKLINE_MACD4',])
-'''
+
 
 
 daily_import_stocks_into_tdx('D:\dan\stock\py_stock\calendar\\2017_week.json',
                              'D:\\new_qlzq_v6\T0002\\blocknew',
                              'D:\dan\stock\py_stock\quantdigger\candicates',
                              {'1DAY': '1D',},
-                             ['MACD_MA7', 'MACD_MA8'],
+                             ['MACD_MA7', 'MACD_MA8', 'MACD_MA9']#['ZT62808DKLINE_MACD4','ZT62808DKLINE_MACD7'] #['MACD_MA7', 'MACD_MA8'],
                              )
+
+'''
+daily_import_stocks_into_ths('D:\dan\stock\py_stock\calendar\\2017_week.json',
+                             'C:\\同花顺软件\\同花顺\\mx_297788656',
+                             'D:\dan\stock\py_stock\quantdigger\candicates',
+                             {'1DAY': '1D',},
+                             ['MACD_MA7', 'MACD_MA8', 'MACD_MA9']#['ZT62808DKLINE_MACD4','ZT62808DKLINE_MACD7'] #['MACD_MA7', 'MACD_MA8'],
+                             )
+
+
+
+
 
